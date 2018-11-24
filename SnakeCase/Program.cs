9ab45d141e9e21e6a1a383f0b15aa91079e53013
ralmsdeveloper <ralms@ralms.net>
2018-11-24 20:52:38 -0300

    Adicionar arquivos de projeto.

---
+++ SnakeCase/Program.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace SnakeCase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var db = new SampleContext())
+            {
+                var script = db.Database.GenerateCreateScript();
+            }  
+        }
+    }
+
+    public sealed class SampleContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseNpgsql(
+                    "Host=127.0.0.1;Username=postgres;Password=XXX;Database=TestSnake", 
+                    _ => _.EnableRetryOnFailure());
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelo)
+        {
+            modelo.Entity<TestSnakeCase>();
+
+            // Aqui está nossa mágica!
+            modelo.ToSnakeNames();
+        }
+    }
+
+    public class TestSnakeCase
+    {
+        public int Id { get; set; }
+        public int CodigoIBGE { get; set; }
+        public string NomeCompleto { get; set; } 
+        public int AnoNascimento { get; set; }
+        public DateTime DataCadastro { get; set; }
+    }
+}
---
+++ SnakeCase/SnakeCase.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Text.RegularExpressions;
+
+namespace SnakeCase
+{
+    public static class SnakeCase
+    {
+        public static void ToSnakeNames(this ModelBuilder modelBuilder)
+        {
+            foreach (var entity in modelBuilder.Model.GetEntityTypes())
+            {
+                entity.Relational().TableName = entity.Relational().TableName.ToSnakeCase();
+
+                foreach (var property in entity.GetProperties())
+                {
+                    property.Relational().ColumnName = property
+                        .Relational()
+                        .ColumnName
+                        .ToSnakeCase();
+                }
+
+                foreach (var key in entity.GetKeys())
+                {
+                    key.Relational().Name = key.Relational().Name.ToSnakeCase();
+                }
+
+                foreach (var key in entity.GetForeignKeys())
+                {
+                    key.Relational().Name = key.Relational().Name.ToSnakeCase();
+                }
+
+                foreach (var index in entity.GetIndexes())
+                {
+                    index.Relational().Name = index.Relational().Name.ToSnakeCase();
+                }
+            }
+        }
+
+        private static string ToSnakeCase(this string name)
+        {
+            return string.IsNullOrWhiteSpace(name)
+                ? name
+                : Regex.Replace(
+                    name,
+                    @"([a-z0-9])([A-Z])",
+                    "$1_$2",
+                    RegexOptions.Compiled,
+                    TimeSpan.FromSeconds(1)).ToLower();
+        }
+    }
+}
