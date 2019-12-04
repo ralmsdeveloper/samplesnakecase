be673a9bca4801aa66576ad2e1182a1599120365
Rafael Almeida <ralms@ralms.net>
2019-12-03 21:24:51 -0300

    EFCore 3.1

--- SnakeCase/Program.cs
+++ SnakeCase/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using System;
+using System.Text.RegularExpressions;
 
 namespace SnakeCase
 {
@@ -7,10 +8,9 @@ namespace SnakeCase
     {
         static void Main(string[] args)
         {
-            using (var db = new SampleContext())
-            {
-                var script = db.Database.GenerateCreateScript();
-            }  
+            using var db = new SampleContext();
+            var script = db.Database.GenerateCreateScript();
+            Console.WriteLine(script);
         }
     }
 
@@ -32,6 +32,54 @@ namespace SnakeCase
 
             // Aqui está nossa mágica!
             modelo.ToSnakeNames();
+        }
+    }
+
+    public static class SnakeCase
+    {
+        public static void ToSnakeNames(this ModelBuilder modelBuilder)
+        {
+            foreach (var entity in modelBuilder.Model.GetEntityTypes())
+            {
+                var tableName = entity.GetTableName().ToSnakeCase();
+                entity.SetTableName(tableName);
+
+                foreach (var property in entity.GetProperties())
+                {
+                    var columnName = property.GetColumnName().ToSnakeCase();
+                    property.SetColumnName(columnName);
+                }
+
+                foreach (var key in entity.GetKeys())
+                {
+                    var keyName = key.GetName().ToSnakeCase();
+                    key.SetName(keyName);
+                }
+
+                foreach (var key in entity.GetForeignKeys())
+                {
+                    var foreignKeyName = key.GetConstraintName().ToSnakeCase();
+                    key.SetConstraintName(foreignKeyName);
+                }
+
+                foreach (var index in entity.GetIndexes())
+                {
+                    var indexName = index.GetName().ToSnakeCase();
+                    index.SetName(indexName);
+                }
+            }
+        }
+
+        private static string ToSnakeCase(this string name)
+        {
+            return string.IsNullOrWhiteSpace(name)
+                ? name
+                : Regex.Replace(
+                    name,
+                    @"([a-z0-9])([A-Z])",
+                    "$1_$2",
+                    RegexOptions.Compiled,
+                    TimeSpan.FromSeconds(1)).ToLower();
         }
     }
 
